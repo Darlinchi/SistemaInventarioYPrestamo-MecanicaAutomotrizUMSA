0767f80c501f99468af6c4ce0ecc90580a6c1fa6
Darlin Nataly Soliz Quispe <darlinnatalysolizquispe@gmail.com>
2026-05-30 19:07:56 -0400

    fix: forzar HTTPS en produccion

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,24 +3,22 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        //
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
+
         Gate::before(function ($user, $ability) {
             if ($user->hasRole('admin')) {
                 return true;
